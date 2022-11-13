21f0f9111e6422a184a53a969d07888a38c5d94f
daniel <dani2msn@gmail.com>
2022-11-12 16:08:52 -0800

    Change the form controls to required

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ odinForm.noValidate = true;
 odinForm.addEventListener("submit", validateForm);
 function validateForm(e) {
     const form = e.target;
-    if (form.checkValidity()) {
-        if (!validatePasswords(form)) e.preventDefault();
-    } else {
+    if (!form.checkValidity()) {
         e.preventDefault();
         [...form.elements].forEach(i => {
             if(i.checkValidity()) {
@@ -16,19 +14,23 @@ function validateForm(e) {
             }
         });
     }
+    if (!validatePasswords(form)) e.preventDefault();
 }
 
 function validatePasswords(form) {
     const passwords = [...form.elements].filter(element => element.type == 'password');
     const password = passwords[0];
     const confirmPassword = passwords[1];
-    if (password.value == confirmPassword.value) {
-        password.parentElement.classList.remove("invalid");
-        confirmPassword.parentElement.classList.remove("invalid");
-        return true;
-    } else {
-        password.parentElement.classList.add("invalid");
-        confirmPassword.parentElement.classList.add("invalid");
-        return false;
+    if (password.value) {
+        if (password.value == confirmPassword.value) {
+            password.parentElement.classList.remove("invalid");
+            confirmPassword.parentElement.classList.remove("invalid");
+            return true;
+        } else {
+            password.parentElement.classList.add("invalid");
+            confirmPassword.parentElement.classList.add("invalid");
+            document.querySelector("input[type='password']+p").textContent = "Passwords do not match";
+            return false;
+        }
     }
 }
